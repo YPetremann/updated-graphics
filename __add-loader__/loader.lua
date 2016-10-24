849a4d8277b357256f2d7f538b048aab64333244
YPetremann <Y.Petremann@gmail.com>
2016-10-24 09:12:30 +0200

    Add loader for major using loader textures

---
+++ __add-loader__/loader.lua
@@ -0,0 +1,12 @@
+merge(overwrite_graphics,{
+  ["__add-loader__/graphics/icons/faster-loader.png"]=replace{width = 96},
+  ["__add-loader__/graphics/icons/extremely-fast-loader.png"]=replace{width = 96},
+  ["__add-loader__/graphics/icons/express-loader.png"]=replace{width = 96},
+  ["__add-loader__/graphics/icons/loader.png"]=replace{width = 96},
+  ["__add-loader__/graphics/icons/fast-loader.png"]=replace{width = 96},
+  ["__add-loader__/graphics/entity/loader/fast-loader-structure.png"]=replace{width = 96},
+  ["__add-loader__/graphics/entity/loader/faster-loader-structure.png"]=replace{width = 96},
+  ["__add-loader__/graphics/entity/loader/extremely-fast-loader-structure.png"]=replace{width = 96},
+  ["__add-loader__/graphics/entity/loader/express-loader-structure.png"]=replace{width = 96},
+  ["__add-loader__/graphics/entity/loader/loader-structure.png"]=replace{width = 96},
+})
---
+++ __base__/loader.lua
@@ -0,0 +1,22 @@
+function custom(r)
+  -- custom
+  return function(n,p,i)
+    if p.type == "loader" then
+      p.structure.direction_in.sheet.filename = "__base__/graphics/entity/loader/"..p.name.."-structure.png"
+      p.structure.direction_out.sheet.filename = "__base__/graphics/entity/loader/"..p.name.."-structure.png"
+    end
+    p[i]=string.gsub(p[i],"(__.[^/]+__)","__"..modname.."__/%1")
+    if r ~= nil then
+      for k,v in pairs(r) do
+        p[k]=v
+      end
+    end
+  end
+end
+
+merge(overwrite_graphics,{
+  ["__base__/graphics/icons/loader.png"]=custom{},
+  ["__base__/graphics/entity/loader/loader-structure.png"]=replace{width = 96},
+  ["__base__/graphics/entity/loader/fast-loader-structure.png"]=replace{width = 96},
+  ["__base__/graphics/entity/loader/express-loader-structure.png"]=replace{width = 96},
+})
