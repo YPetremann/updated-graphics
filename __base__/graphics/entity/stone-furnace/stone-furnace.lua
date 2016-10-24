5ac06b436effa99cf3dc0b620ed83efe6d2592ac
YPetremann <Y.Petremann@gmail.com>
2016-10-24 09:22:09 +0200

    use old new furnace

---
+++ __base__/graphics/entity/stone-furnace/stone-furnace.lua
@@ -0,0 +1,14 @@
+merge(overwrite_graphics,{
+	["__base__/graphics/entity/stone-furnace/stone-furnace.png"]=replace{width=91,height=69},
+	["__base__/graphics/entity/stone-furnace/stone-furnace-fire.png"]=replace{
+		width = 25,
+		height = 24,
+		line_length = 12,
+		frame_count = 12,
+		axially_symmetrical = false,
+		direction_count = 1,
+		shift = {0.0625, 0.6},
+        animation_speed = 0.25
+	},
+})
+ 
